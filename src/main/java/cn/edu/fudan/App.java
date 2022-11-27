d047bbf1b95edf06e82fe4152551e6febe18528c
TangShangxuan <20302010028@fudan.edu.cn>
2022-11-27 21:31:40 +0800

    Initial commit

---
+++ src/main/java/cn/edu/fudan/App.java
@@ -0,0 +1,13 @@
+package cn.edu.fudan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
