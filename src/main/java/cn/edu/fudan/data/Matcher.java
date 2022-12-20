8f569ea4cb345f895a61dfe86b92ce79345526da
ZhuYuchen <20302010020@fudan.edu.cn>
2022-12-20 20:57:37 +0800

    closed status set

--- src/main/java/cn/edu/fudan/data/Matcher.java
+++ src/main/java/cn/edu/fudan/data/Matcher.java
@@ -37,23 +37,70 @@ public class Matcher {
             conn = DBConnection.getConn();
             String sql1 = "select id,type,message FROM instance where commit_id=?;";
             ResultSet rs1;
-            if(parent_commit_id!=-1){
-                ps = conn.prepareStatement(sql1);
-                ps.setInt(1,parent_commit_id);
-                rs1 = ps.executeQuery();
-                while(rs1.next()){
-                    RawIssue preRawIssue1 = new RawIssue();
-                    int inst_id=rs1.getInt("id");
-                    String type=rs1.getString("type");
-                    String detail=rs1.getString("message");
+            try{
+                if(parent_commit_id!=-1){
+                    ps = conn.prepareStatement(sql1);
+                    ps.setInt(1,parent_commit_id);
+                    rs1 = ps.executeQuery();
+                    while(rs1.next()){
+                        RawIssue preRawIssue1 = new RawIssue();
+                        int inst_id=rs1.getInt("id");
+                        String type=rs1.getString("type");
+                        String detail=rs1.getString("message");
 
-                    preRawIssue1.setUuid(""+inst_id);
-                    preRawIssue1.setType(type);
+                        preRawIssue1.setUuid(""+inst_id);
+                        preRawIssue1.setType(type);
 
-                    preRawIssue1.setDetail(detail);
-                    preRawIssue1.setCommitId(""+parent_commit_id);
+                        preRawIssue1.setDetail(detail);
+                        preRawIssue1.setCommitId(""+parent_commit_id);
 
-                    Location preLocation1 = new Location();
+                        Location preLocation1 = new Location();
+
+                        String loc_sql="select component,start_line,end_line,start_offset FROM location where inst_id=?";
+                        PreparedStatement ps_loc = conn.prepareStatement(loc_sql);
+                        ps_loc.setInt(1,inst_id);
+                        ResultSet loc_rs = ps_loc.executeQuery();
+                        if(loc_rs.next()){
+                            String component=loc_rs.getString("component");
+                            preRawIssue1.setFileName(component);
+                            int start_line=loc_rs.getInt("start_line");
+                            int end_line=loc_rs.getInt("end_line");
+                            int start_offset=loc_rs.getInt("start_offset");
+                            preLocation1.setStartLine(start_line);
+                            preLocation1.setEndLine(end_line);
+                            preLocation1.setStartToken(start_offset);
+                            preRawIssue1.setLocations(Collections.singletonList(preLocation1));
+                        }
+                        else{
+                            break;
+                        }
+                        System.out.println("add_a_new_preiss");
+                        preRawIssueList.add(preRawIssue1);
+                    }
+                }
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                String sql2 = "select id,type,message,status FROM instance where commit_id=?;";
+                ps = conn.prepareStatement(sql2);
+                ps.setInt(1,commit.getId());
+                ResultSet rs2 = ps.executeQuery();
+                while(rs2.next()){
+
+                    RawIssue curRawIssue1 = new RawIssue();
+                    int inst_id=rs2.getInt("id");
+                    String type=rs2.getString("type");
+                    String detail=rs2.getString("message");
+                    String status=rs2.getString("status");
+                    curRawIssue1.setUuid(""+inst_id);
+                    curRawIssue1.setType(type);
+
+                    curRawIssue1.setDetail(detail);
+                    curRawIssue1.setCommitId(""+parent_commit_id);
+                    curRawIssue1.setStatus(status);
+
+                    Location curLocation1 = new Location();
 
                     String loc_sql="select component,start_line,end_line,start_offset FROM location where inst_id=?";
                     PreparedStatement ps_loc = conn.prepareStatement(loc_sql);
@@ -61,61 +108,22 @@ public class Matcher {
                     ResultSet loc_rs = ps_loc.executeQuery();
                     if(loc_rs.next()){
                         String component=loc_rs.getString("component");
-                        preRawIssue1.setFileName(component);
+                        curRawIssue1.setFileName(component);
                         int start_line=loc_rs.getInt("start_line");
                         int end_line=loc_rs.getInt("end_line");
                         int start_offset=loc_rs.getInt("start_offset");
-                        preLocation1.setStartLine(start_line);
-                        preLocation1.setEndLine(end_line);
-                        preLocation1.setStartToken(start_offset);
-                        preRawIssue1.setLocations(Collections.singletonList(preLocation1));
+                        curLocation1.setStartLine(start_line);
+                        curLocation1.setEndLine(end_line);
+                        curLocation1.setStartToken(start_offset);
+                        curRawIssue1.setLocations(Collections.singletonList(curLocation1));
                     }
-                    else{
-                        break;
-                    }
-                    System.out.println("add_a_new_preiss");
-                    preRawIssueList.add(preRawIssue1);
+                    System.out.println("add_a_new_curiss");
+                    curRawIssueList.add(curRawIssue1);
                 }
+            }catch (SQLException e) {
+                e.printStackTrace();
             }
 
-
-            String sql2 = "select id,type,message,status FROM instance where commit_id=?;";
-            ps = conn.prepareStatement(sql2);
-            ps.setInt(1,commit.getId());
-            ResultSet rs2 = ps.executeQuery();
-            while(rs2.next()){
-                RawIssue curRawIssue1 = new RawIssue();
-                int inst_id=rs2.getInt("id");
-                String type=rs2.getString("type");
-                String detail=rs2.getString("message");
-                String status=rs2.getString("status");
-                curRawIssue1.setUuid(""+inst_id);
-                curRawIssue1.setType(type);
-
-                curRawIssue1.setDetail(detail);
-                curRawIssue1.setCommitId(""+parent_commit_id);
-                curRawIssue1.setStatus(status);
-
-                Location curLocation1 = new Location();
-
-                String loc_sql="select component,start_line,end_line,start_offset FROM location where inst_id=?";
-                PreparedStatement ps_loc = conn.prepareStatement(loc_sql);
-                ps_loc.setInt(1,inst_id);
-                ResultSet loc_rs = ps_loc.executeQuery();
-                if(loc_rs.next()){
-                    String component=loc_rs.getString("component");
-                    curRawIssue1.setFileName(component);
-                    int start_line=loc_rs.getInt("start_line");
-                    int end_line=loc_rs.getInt("end_line");
-                    int start_offset=loc_rs.getInt("start_offset");
-                    curLocation1.setStartLine(start_line);
-                    curLocation1.setEndLine(end_line);
-                    curLocation1.setStartToken(start_offset);
-                    curRawIssue1.setLocations(Collections.singletonList(curLocation1));
-                }
-                System.out.println("add_a_new_curiss");
-                curRawIssueList.add(curRawIssue1);
-            }
             InstcaseDAO instcaseDAO=new InstcaseDAO();
             if(preRawIssueList.isEmpty()){
                 System.out.println("NnParent");
@@ -164,84 +172,89 @@ public class Matcher {
                     instcaseDAO.insert(instcase);
                 }
                 else{
-                    String loc_sql="select id,create_time,commit_new,committer_new FROM instcase where inst_last=?";
-                    ps = conn.prepareStatement(loc_sql);
-                    ps.setInt(1,Integer.parseInt(rawIssue.getMappedRawIssue().getUuid()));
-                    ResultSet loc_rs = ps.executeQuery();
-                    InstCase instcase=new InstCase();
-                    if(loc_rs.next()){
-                        int id=loc_rs.getInt("id");
-                        Timestamp create_time=loc_rs.getTimestamp("create_time");
-                        int commit_new=loc_rs.getInt("commit_new");
-                        String committer_new=loc_rs.getString("committer_new");
-                        instcase.setCommitNew(commit_new);
-                        instcase.setStatus(rawIssue.getStatus());
-                        instcase.setType(rawIssue.getType());
-                        instcase.setUpdateTime(commit.getCommitTime());
-                        instcase.setCommitterNew(committer_new);
-                        instcase.setCommitterLast(commit.getCommitter());
-                        instcase.setId(id);
-                        instcase.setCreateTime(create_time);
-                        instcase.setDurationTime((int)((commit.getCommitTime().getTime()-create_time.getTime())/1000));
-                        instcase.setCommitLast(commit.getId());
-                        instcase.setInstLast(Integer.parseInt(rawIssue.getUuid()));
+                    try{
+                        String loc_sql="select id,create_time,commit_new,committer_new FROM instcase where inst_last=?";
+                        ps = conn.prepareStatement(loc_sql);
+                        ps.setInt(1,Integer.parseInt(rawIssue.getMappedRawIssue().getUuid()));
+                        ResultSet loc_rs = ps.executeQuery();
+                        InstCase instcase=new InstCase();
+                        if(loc_rs.next()){
+                            int id=loc_rs.getInt("id");
+                            Timestamp create_time=loc_rs.getTimestamp("create_time");
+                            int commit_new=loc_rs.getInt("commit_new");
+                            String committer_new=loc_rs.getString("committer_new");
+                            instcase.setCommitNew(commit_new);
+                            instcase.setStatus(rawIssue.getStatus());
+                            instcase.setType(rawIssue.getType());
+                            instcase.setUpdateTime(commit.getCommitTime());
+                            instcase.setCommitterNew(committer_new);
+                            instcase.setCommitterLast(commit.getCommitter());
+                            instcase.setId(id);
+                            instcase.setCreateTime(create_time);
+                            instcase.setDurationTime((int)((commit.getCommitTime().getTime()-create_time.getTime())/1000));
+                            instcase.setCommitLast(commit.getId());
+                            instcase.setInstLast(Integer.parseInt(rawIssue.getUuid()));
+                        }
+                        else {
+                            System.out.println("Instcase not found, while parent instance exists.");
+                            instcase.setCommitLast(commit.getId());
+                            instcase.setCommitNew(commit.getId());
+                            instcase.setStatus(rawIssue.getStatus());
+                            instcase.setType(rawIssue.getType());
+                            instcase.setCreateTime(commit.getCommitTime());
+                            instcase.setUpdateTime(commit.getCommitTime());
+                            instcase.setCommitterNew(commit.getCommitter());
+                            instcase.setCommitterLast(commit.getCommitter());
+                            instcase.setDurationTime(0);
+                            instcase.setInstLast(Integer.parseInt(rawIssue.getUuid()));
+                            instcaseDAO.insert(instcase);
+                        }
+                        instcaseDAO.update(instcase);
                     }
-                    else {
-                        System.out.println("Instcase not found, while parent instance exists.");
-                        instcase.setCommitLast(commit.getId());
-                        instcase.setCommitNew(commit.getId());
-                        instcase.setStatus(rawIssue.getStatus());
-                        instcase.setType(rawIssue.getType());
-                        instcase.setCreateTime(commit.getCommitTime());
-                        instcase.setUpdateTime(commit.getCommitTime());
-                        instcase.setCommitterNew(commit.getCommitter());
-                        instcase.setCommitterLast(commit.getCommitter());
-                        instcase.setDurationTime(0);
-                        instcase.setInstLast(Integer.parseInt(rawIssue.getUuid()));
-                        instcaseDAO.insert(instcase);
+                    catch (SQLException e) {
+                        e.printStackTrace();
                     }
-
-
                     Match match=new Match();
                     match.setParentId(Integer.parseInt(rawIssue.getMappedRawIssue().getUuid()));
                     match.setChildId(Integer.parseInt(rawIssue.getUuid()));
                     MatchDAO m=new MatchDAO();
                     m.insert(match);
-
-                    instcaseDAO.update(instcase);
                 }
             }
             for (RawIssue rawIssue : preRawIssueList) {
                 if(rawIssue.getMappedRawIssue()==null){
-                    String loc_sql="select id,create_time,commit_new,committer_new,committer_last FROM instcase where inst_last=?";
-                    ps = conn.prepareStatement(loc_sql);
-                    ps.setInt(1,Integer.parseInt(rawIssue.getMappedRawIssue().getUuid()));
-                    ResultSet loc_rs = ps.executeQuery();
-                    loc_rs.next();
-                    int id=loc_rs.getInt("id");
-                    Timestamp create_time=loc_rs.getTimestamp("create_time");
-                    int commit_new=loc_rs.getInt("commit_new");
-                    String committer_new=loc_rs.getString("committer_new");
-                    String committer_last=loc_rs.getString("committer_last");
-                    InstCase instcase=new InstCase();
-                    instcase.setCommitNew(commit_new);
-                    instcase.setStatus("CLOSED");
-                    instcase.setType(rawIssue.getType());
-                    instcase.setUpdateTime(commit.getCommitTime());
-                    instcase.setCommitterNew(committer_new);
-                    instcase.setCommitterLast(committer_last);
-                    instcase.setId(id);
-                    instcase.setCreateTime(create_time);
-                    instcase.setDurationTime((int)((commit.getCommitTime().getTime()-create_time.getTime())/1000));
-                    instcase.setCommitLast(parent_commit_id);
-                    instcase.setInstLast(Integer.parseInt(rawIssue.getUuid()));
+                    try{
+                        String loc_sql="select id,create_time,commit_new,committer_new,committer_last FROM instcase where inst_last=?";
+                        ps = conn.prepareStatement(loc_sql);
+                        ps.setInt(1,Integer.parseInt(rawIssue.getUuid()));
+                        ResultSet loc_rs = ps.executeQuery();
+                        loc_rs.next();
+                        int id=loc_rs.getInt("id");
+                        Timestamp create_time=loc_rs.getTimestamp("create_time");
+                        int commit_new=loc_rs.getInt("commit_new");
+                        String committer_new=loc_rs.getString("committer_new");
+                        String committer_last=loc_rs.getString("committer_last");
+                        InstCase instcase=new InstCase();
+                        instcase.setCommitNew(commit_new);
+                        instcase.setStatus("CLOSED");
+                        instcase.setType(rawIssue.getType());
+                        instcase.setUpdateTime(commit.getCommitTime());
+                        instcase.setCommitterNew(committer_new);
+                        instcase.setCommitterLast(committer_last);
+                        instcase.setId(id);
+                        instcase.setCreateTime(create_time);
+                        instcase.setDurationTime((int)((commit.getCommitTime().getTime()-create_time.getTime())/1000));
+                        instcase.setCommitLast(parent_commit_id);
+                        instcase.setInstLast(Integer.parseInt(rawIssue.getUuid()));
 
-                    instcaseDAO.update(instcase);
+                        instcaseDAO.update(instcase);
+                    }catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
                 }
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        }  catch (IOException e) {
             e.printStackTrace();
         }
         finally {
